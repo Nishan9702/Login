66c4e51d3c345eedcd19673e446512f3a37c7b8f
Nishan Rai <nishansampang9@gmail.com>
2026-03-23 09:30:04 +0545

    Adding user's getter setter

--- src/main/java/controller/LoginController.java
+++ src/main/java/controller/LoginController.java
@@ -1,31 +1,31 @@
-package controller;
-
-import view.LoginView;
-import dao.UserDAO;
-
-import javax.swing.*;
-
-public class LoginController {
-
-    private LoginView view;
-    private UserDAO dao;
-
-    public LoginController(LoginView view, UserDAO dao) {
-        this.view = view;
-        this.dao = dao;
-
-        this.view.loginButton.addActionListener(e -> login());
-    }
-
-    private void login() {
-
-        String username = view.usernameField.getText();
-        String password = new String(view.passwordField.getPassword());
-
-        if (dao.validateUser(username, password)) {
-            JOptionPane.showMessageDialog(view, "Login Successful!");
-        } else {
-            JOptionPane.showMessageDialog(view, "Invalid Username or Password");
-        }
-    }
-}
+//package controller;
+//
+//import view.LoginView;
+//import dao.UserDAO;
+//
+//import javax.swing.*;
+//
+//public class LoginController {
+//
+//    private LoginView view;
+//    private UserDAO dao;
+//
+//    public LoginController(LoginView view, UserDAO dao) {
+//        this.view = view;
+//        this.dao = dao;
+//
+//        this.view.loginButton.addActionListener(e -> login());
+//    }
+//
+//    private void login() {
+//
+//        String username = view.usernameField.getText();
+//        String password = new String(view.passwordField.getPassword());
+//
+//        if (dao.validateUser(username, password)) {
+//            JOptionPane.showMessageDialog(view, "Login Successful!");
+//        } else {
+//            JOptionPane.showMessageDialog(view, "Invalid Username or Password");
+//        }
+//    }
+//}
